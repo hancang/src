d4ec4910c25bfa29226e5e4757d9e16a3ea585e7
huck.zhou <hancang.zhou@bybit.com>
2022-12-20 16:12:37 +0800

    yes

--- hancangzhou/hello/hello.go
+++ hancangzhou/hello/hello.go
@@ -14,7 +14,7 @@ func main() {
 	log.SetFlags(0)
 
 	// Request a greeting message.
-	message, err := greetings.Hello("hancang")
+	message, err := greetings.Hello("")
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
